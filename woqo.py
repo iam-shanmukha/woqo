4f862d68d644de099c1a7b9424d6c3153a528fe6
iam-shanmukha <vishnushanmukha@gmail.com>
2020-08-14 21:09:38 +0530

    WOKO

--- woqo.py
+++ woqo.py
@@ -3,6 +3,20 @@ from datetime import date
 from bs4 import BeautifulSoup
 today = date.today()
 quotes=[]
+td = today.day
+tm = today.month
+ty = today.year
+### DICTIONARY.COM ###
+
+print(r'''
+	                _            
+__      __   ___   | | __   ___  
+\ \ /\ / /  / _ \  | |/ /  / _ \ 
+ \ V  V /  | (_) | |   <  | (_) |
+  \_/\_/    \___/  |_|\_\  \___/ 
+        @github.com/iam-shanmukha/
+
+	''')
 
 word = "https://www.dictionary.com/e/word-of-the-day/"
 word_page = requests.get(word)
@@ -10,16 +24,18 @@ word_soup = BeautifulSoup(word_page.content,'html.parser')
 view_word = word_soup.find('title')
 view_word_def  = word_soup.find("div", {"class": "otd-item-headword__pos-blocks"})
 
+### BRAINYQUOTE.COM ###
 brainyquote = "https://www.brainyquote.com/quote_of_the_day"
 brain_page = requests.get(brainyquote)
 brain_soup = BeautifulSoup(brain_page.content, 'html.parser')
 view_quote  = brain_soup.find_all(title="view quote")
-
 #class="otd-item-headword__pos-blocks"
-print("########### WORD OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
-print(view_word.string[:-15])
-print(view_word_def.text)
-print("########### QUOTE OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
+
+print("########### WORD OF THE DAY - {}/{}/{} ###########\n".format(td,tm,ty))
+print(view_word.string[18:-16].strip())
+print(view_word_def.text.strip())
+print()
+print("########### QUOTE OF THE DAY - {}/{}/{} ###########\n".format(td,tm,ty))
 for quote in view_quote:
 	quotes.append(quote.text)
 print(quotes[1])
