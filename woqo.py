d814d981f8084f53550e5b885ae14033f2293d0e
iam-shanmukha <vishnushanmukha@gmail.com>
2020-08-14 20:37:39 +0530

    ADDED WORD

--- woqo.py
+++ woqo.py
@@ -3,10 +3,22 @@ from datetime import date
 from bs4 import BeautifulSoup
 today = date.today()
 quotes=[]
-url = "https://www.brainyquote.com/quote_of_the_day"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-view_quote  = soup.find_all(title="view quote")
+
+word = "https://www.dictionary.com/e/word-of-the-day/"
+word_page = requests.get(word)
+word_soup = BeautifulSoup(word_page.content,'html.parser')
+view_word = word_soup.find('title')
+view_word_def  = word_soup.find("div", {"class": "otd-item-headword__pos-blocks"})
+
+brainyquote = "https://www.brainyquote.com/quote_of_the_day"
+brain_page = requests.get(brainyquote)
+brain_soup = BeautifulSoup(brain_page.content, 'html.parser')
+view_quote  = brain_soup.find_all(title="view quote")
+
+#class="otd-item-headword__pos-blocks"
+print("########### WORD OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
+print(view_word.string[:-15])
+print(view_word_def.text)
 print("########### QUOTE OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
 for quote in view_quote:
 	quotes.append(quote.text)
