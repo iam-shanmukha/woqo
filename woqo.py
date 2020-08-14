752de28ae9d26827ebe32bd1d03af0259ef43467
iam-shanmukha <vishnushanmukha@gmail.com>
2020-08-14 20:05:02 +0530

    showing one quote only

--- woqo.py
+++ woqo.py
@@ -2,19 +2,12 @@ import requests
 from datetime import date
 from bs4 import BeautifulSoup
 today = date.today()
-try:	
-	with open("woqo.txt", 'r') as f:
-		line = f.readlines()[-1]
-	count = int(line[0])
-
-except FileNotFoundError:
-	count = 1
+quotes=[]
 url = "https://www.brainyquote.com/quote_of_the_day"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 view_quote  = soup.find_all(title="view quote")
-with open("woqo.txt","a") as file:
-	file.write("########### QUOTE OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
-	for quote in view_quote:
-		file.write("{} {} {}".format(count,quote.text,"\n"))
-		count = count+1
+print("########### QUOTE OF THE DAY - {}/{}/{} ###########\n".format(today.day,today.month,today.year))
+for quote in view_quote:
+	quotes.append(quote.text)
+print(quotes[1])
